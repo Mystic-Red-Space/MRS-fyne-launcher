11b7bfd5d80642257144dc7ad440fd4b998a4dc7
Socio-path <doohee006@gmail.com>
2019-10-30 00:49:30 +0900

    Add icon

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
+	"io/ioutil"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
 	application := app.New()
-
+	image, err := ioutil.ReadFile("icon.png")
+	check(err)
+	icon := fyne.NewStaticResource("icon",image)
+	application.SetIcon(icon)
 	w := application.NewWindow("Hello")
 	w.SetContent(widget.NewVBox(
 		widget.NewLabel("Hello Fyne!"),
