b0c57792d8e423d3b4717b00d859db122656ff14
Socio-path <doohee006@gmail.com>
2019-11-09 23:44:15 +0900

    Edit ui error

--- main.go
+++ main.go
@@ -1,110 +1,112 @@
 package main
 
 import (
-    "encoding/json"
-    "fyne.io/fyne"
-    "fyne.io/fyne/app"
-    "fyne.io/fyne/layout"
-    "fyne.io/fyne/theme"
-    "fyne.io/fyne/widget"
-    "io/ioutil"
-    "net/http"
+	"encoding/json"
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/theme"
+	"fyne.io/fyne/widget"
+	"io/ioutil"
+	"net/http"
 )
 
 type launcher struct {
-    functions map[string]func()
-    modpacks  *widget.Select
-    output    *widget.Label
-    buttons   map[string]*widget.Button
-    window    fyne.Window
+	functions map[string]func()
+	modpacks  *widget.Select
+	output    *widget.Label
+	buttons   map[string]*widget.Button
+	window    fyne.Window
 }
 type mcfile struct {
-    Name string `json:"name"`
-    Dir string `json:"dir"`
-    Url string `json:"url"`
-    Md5 string `json:"md5"`
+	Name string `json:"name"`
+	Dir  string `json:"dir"`
+	Url  string `json:"url"`
+	Md5  string `json:"md5"`
 }
 
 type modpack struct {
-    Name string `json:"name"`
-    Icon string `json:"icon"`
-    Profile string `json:"profile"`
+	Name    string `json:"name"`
+	Icon    string `json:"icon"`
+	Profile string `json:"profile"`
 }
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func getJson(url string, format interface{}) {
-    resp, _ := http.Get(url)
-    res, _ := ioutil.ReadAll(resp.Body)
-    _ = json.Unmarshal(res, &format)
+	resp, _ := http.Get(url)
+	res, _ := ioutil.ReadAll(resp.Body)
+	_ = json.Unmarshal(res, &format)
 }
 
 func newLauncher() *launcher {
-    c := &launcher{}
-    c.functions = make(map[string]func())
-    c.buttons = make(map[string]*widget.Button)
+	c := &launcher{}
+	c.functions = make(map[string]func())
+	c.buttons = make(map[string]*widget.Button)
 
-    return c
+	return c
 }
 
 func (c *launcher) addButton(text string, action func()) *widget.Button {
-    button := widget.NewButton(text, action)
-    c.buttons[text] = button
+	button := widget.NewButton(text, action)
+	c.buttons[text] = button
 
-    return button
+	return button
 }
 
 func (c *launcher) loadUI(app fyne.App) {
-    c.output = widget.NewLabel("Hello World!")
-    var modpacks []modpack
-    modpacknames := make([]string, 0, 5)
-    getJson("https://api.mysticrs.tk/list", &modpacks)
-    for _, modp := range modpacks {
-        modpacknames = append(modpacknames, modp.Name)
-    }
-    c.modpacks = widget.NewSelect(modpacknames, func(s string) {
-        c.output.SetText(s)
-    })
-    c.window = app.NewWindow("MRS Launcher")
-    c.window.SetIcon(resourceIconPng)
-    c.window.SetContent(
-        fyne.NewContainerWithLayout(
-            layout.NewBorderLayout(
-                nil,
-                nil,
-                fyne.NewContainerWithLayout(
-                    layout.NewGridLayout(1),
-                    c.addButton("Home", func() {
-                        c.window.SetFullScreen(!c.window.FullScreen())
-                    }),
-                    c.addButton("Modpacks", func() {
-                        app.Settings().SetTheme(theme.LightTheme())
-                    }),
-                    c.addButton("Settings", func() {
-                        app.Settings().SetTheme(theme.DarkTheme())
-                    }),
-                ),
-                nil,
-            ),
-            c.buttons["Home"],
-            c.buttons["Modpacks"],
-            c.buttons["Settings"],
-            fyne.NewContainerWithLayout(
-                layout.NewGridLayout(1),
-                c.modpacks,
-                c.output,
-            ),
-        ),
-    )
-    c.modpacks.SetSelected(modpacknames[0])
-    c.window.ShowAndRun()
+	c.output = widget.NewLabel("Hello World!")
+	var modpacks []modpack
+	modpacknames := make([]string, 0, 5)
+	getJson("https://api.mysticrs.tk/list", &modpacks)
+	for _, modp := range modpacks {
+		modpacknames = append(modpacknames, modp.Name)
+	}
+	c.modpacks = widget.NewSelect(modpacknames, func(s string) {
+		c.output.SetText(s)
+	})
+	c.window = app.NewWindow("MRS Launcher")
+	c.window.SetIcon(resourceIconPng)
+	c.addButton("Home", func() {
+		c.window.SetFullScreen(!c.window.FullScreen())
+	})
+	c.addButton("Modpacks", func() {
+		app.Settings().SetTheme(theme.LightTheme())
+	})
+	c.addButton("Settings", func() {
+		app.Settings().SetTheme(theme.DarkTheme())
+	})
+	sidebar := fyne.NewContainerWithLayout(
+		layout.NewGridLayout(1),
+		c.buttons["Home"],
+		c.buttons["Modpacks"],
+		c.buttons["Settings"],
+	)
+	c.window.SetContent(
+		fyne.NewContainerWithLayout(
+			layout.NewBorderLayout(
+				nil,
+				nil,
+				sidebar,
+				nil,
+			),
+			sidebar,
+			fyne.NewContainerWithLayout(
+				layout.NewGridLayout(1),
+				c.modpacks,
+				c.output,
+			),
+		),
+	)
+	c.modpacks.SetSelected(modpacknames[0])
+	c.window.ShowAndRun()
 }
 func main() {
-    application := app.New()
-    c := newLauncher()
-    c.loadUI(application)
+	application := app.New()
+	c := newLauncher()
+	c.loadUI(application)
 }
